2144979f7e4edf3ee0468aa6cb2cd7f0f7d78c06
Primak05nastia <90215957+Primak05nastia@users.noreply.github.com>
2021-11-26 16:53:10 +0200

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -3,18 +3,33 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    int count = 0;
     cin >> n;
     
     int arr[n];
+    int count = 0;
     
-    for(int i = 0; i < n; ++i) {
+    for(int i = 0; i < n; ++i) 
+    {
         cin >> arr[i];
     }
-    for(int i = n - 1; i > -1; --i) {
-        if(arr[i] < 0) {
+    
+    for(int i = n - 1; i > -1; --i) 
+    {
+        if(arr[i] < 0) 
+        {
+            count ++;
+        }
+    }
+    
+    cout << count << "\n";
+    
+    for(int i = n - 1; i > -1; --i) 
+    {
+        if(arr[i] < 0)
+        {
             cout << arr[i] << " ";
         }
     }
